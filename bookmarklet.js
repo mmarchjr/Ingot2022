33230d08692481358d7aee6fe8aa6c11d4b44832
MMarchjr <103056947+mmarchjr@users.noreply.github.com>
2023-01-27 13:02:28 -0600

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-
+javascript:(function () {var a = document.createElement('script');a.src = 'https://cdn.jsdelivr.net/gh/mmarchjr/Ingot2022/Ingot-source-unminified.min.js';document.body.appendChild(a);}())
